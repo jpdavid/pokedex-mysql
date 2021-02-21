8dc0f0d05656f2b8bdccd622fa24357500d367b9
jpdavid <justinepdavid@gmail.com>
2021-02-20 17:10:35 -0800

    Connected and seeded pokedex database

--- server/db/index.js
+++ server/db/index.js
@@ -1,1 +1,12 @@
 // TODO: Establish connection to mysql database
+const mysql = require('mysql');
+
+let connection = mysql.createConnection({
+  user: 'root',
+  password: '',
+  database: 'pokedex'
+});
+
+connection.connect();
+
+module.exports = connection;
