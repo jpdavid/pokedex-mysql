d872ce3955a6bd932f588a235319a36e001df7f2
jpdavid <justinepdavid@gmail.com>
2021-02-20 16:56:16 -0800

    Built express server; created start scripts for server and webpack; Refactored client to dynamically render from react

--- server/index.js
+++ server/index.js
@@ -1,1 +1,19 @@
 // TODO: Create an express server
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const server = express();
+const port = 3000;
+
+server.use(morgan('dev'));
+server.use(cors());
+server.use(bodyParser.json());
+
+server.use(express.static(path.join(__dirname, '../client/dist')));
+//Routes here//
+//server.use('/api')
+
+server.listen(port, () => {console.log(`Listening on port ${port}`)});
